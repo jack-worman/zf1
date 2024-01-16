cd86f6a4e764cf2c7d56861c431dbaee7118f97e
Jack Worman <jworman@healthcall.com>
2024-01-16 10:40:42 -0500

    PHP 8.3

--- tests/Zend/AllTests.php
+++ tests/Zend/AllTests.php
@@ -65,7 +65,6 @@ require_once 'Zend/LocaleTest.php';
 require_once 'Zend/Locale/AllTests.php';
 require_once 'Zend/Log/AllTests.php';
 require_once 'Zend/Mail/AllTests.php';
-require_once 'Zend/Markup/AllTests.php';
 require_once 'Zend/Measure/AllTests.php';
 require_once 'Zend/Memory/AllTests.php';
 require_once 'Zend/MimeTest.php';
@@ -211,7 +210,6 @@ class Zend_AllTests
         $suite->addTest(Zend_Locale_AllTests::suite());
         $suite->addTest(Zend_Log_AllTests::suite());
         $suite->addTest(Zend_Mail_AllTests::suite());
-        $suite->addTest(Zend_Markup_AllTests::suite());
         $suite->addTest(Zend_Measure_AllTests::suite());
         $suite->addTestSuite('Zend_MimeTest');
         $suite->addTest(Zend_Mime_AllTests::suite());
